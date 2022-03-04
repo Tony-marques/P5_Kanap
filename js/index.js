3da741baee610c47516eab887c5274d915b5e296
Tony Marques <tony.marques@live.fr>
2022-03-04 17:09:23 +0100

    v1

---
+++ js/index.js
@@ -0,0 +1,32 @@
+(async () => {
+  const products = await getProducts();
+  console.log(products);
+  displayProducts(products);
+})();
+
+const result = document.querySelector("#items");
+
+// Récupérer les produits de l'API
+async function getProducts() {
+  const response = await fetch("http://localhost:3000/api/products");
+  const body = await response.json();
+  return body;
+}
+
+// Afficher les produits
+function displayProducts(products) {
+  const product = products
+    .map((product) => {
+      return `
+    <a href="./product.html?id=${product._id}">
+    <article>
+      <img src="${product.imageUrl}" alt="${product.description}">
+      <h3 class="${product.name}">${product.name}</h3>
+      <p class="${product.description}">${product.description}</p>
+    </article>
+  </a>
+    `;
+    })
+    .join("");
+  result.innerHTML = product;
+}
