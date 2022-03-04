bf9a1ec6095f040a0f0042e91e404eb74c712a25
Tony Marques <tony.marques@live.fr>
2022-03-04 17:32:03 +0100

    ajout backend

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ const result = document.querySelector("#items");
 // Récupérer les produits de l'API
 async function getProducts() {
   // const response = await fetch("http://localhost:3000/api/products");
+  // test
   const response = await fetch(
     "https://p5-kanap-ocr.herokuapp.com/api/products"
   );
